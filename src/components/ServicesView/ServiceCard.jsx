2504a9bfd52f880ebe4d0734abae4acef5a14a6d
imenvyx <97991438+imenvyx@users.noreply.github.com>
2023-06-28 00:54:54 -0400

    Update ServiceCard.jsx

--- src/components/ServicesView/ServiceCard.jsx
+++ src/components/ServicesView/ServiceCard.jsx
@@ -8,7 +8,7 @@ function IconVariant({ variant }) {
   if (variant === "mobile")
     return (
       <IconMobile
-        className="mb-[18px]"
+        className="mb-[14px]"
         width={" 101.03px"}
         height={" 100.752px"}
         fill="#2E53A3"
@@ -17,7 +17,7 @@ function IconVariant({ variant }) {
   else if (variant === "data_config")
     return (
       <IconDataCC
-        className="mb-[18px]"
+        className="mb-[14px]"
         width={" 101.03px"}
         height={" 100.752px"}
         fill="#2E53A3"
@@ -26,7 +26,7 @@ function IconVariant({ variant }) {
   else
     return (
       <IconConfig
-        className="mb-[18px]"
+        className="mb-[14px]"
         width={" 101.03px"}
         height={" 100.752px"}
         fill="#2E53A3"
@@ -38,18 +38,18 @@ function ServiceCard({ service }) {
   const { icon, title, description, link } = service;
 
   return (
-    <div className="flex flex-col justify-center px-6 w-[286px] h-[366px] bg-[#fff] shadow-[0_0_14px_0_rgba(11,27,0,0.10)]">
+    <div className="flex flex-col  p-8 w-[70%] h-[366px] bg-[#fff] shadow-[0_0_14px_0_rgba(11,27,0,0.10)]">
       <IconVariant variant={icon} />
-      <h4 className="text-[#0F0F0F] mb-[18px] text-[22px] text-[Inter] font-semibold leading-[124.523%]">
+      <h4 className="text-[#0F0F0F] mb-[14px] text-[22px] text-[Inter] font-semibold leading-[124.523%]">
         {title}
       </h4>
 
-      <p className="text-[#B7B7B7] text-[14px] text-[Inter]  leading-[124.523%]">
+      <p className="text-[#B7B7B7] text-[14px] mb-[14px] text-[Inter]  leading-[124.523%]">
         {description}
       </p>
       <div>
         <a href={link} target="_blank" rel="noreferrer">
-          Ver Más
+          Lee Más
         </a>
       </div>
       {/* Leer Mas cuando esta activo */}
